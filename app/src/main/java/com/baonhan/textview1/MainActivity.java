f3f196634e974f43d7baebdcb4913a8da5b6e6f1
4chai <baonhanvtvn97@gmail.com>
2023-04-29 20:58:46 +0700

    xulyview

--- app/src/main/java/com/baonhan/textview1/MainActivity.java
+++ app/src/main/java/com/baonhan/textview1/MainActivity.java
@@ -4,33 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity
     {
+        TextView tvRandom, tvHistory;
+        Button btnReset, btnRandom;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        Random random = new Random();
-//        Math.random();
-//        `1.làm tròn 0.5
-        int number1 = Math.round(0.5f);
-//         2. làm tròn xuống
-        double number2 = Math.floor(1.99999);
-//         3. làm tròn lên
-        double number3 = Math.ceil(1.00001);
-//         4. Random
-        Random random = new Random();
-        for(int i = 0; i < 50; i++){
-            int number4 = random.nextInt(13 -5 +1) + 5;
-//      Log dữ liệu
-        Log.d("BBB",number4 + "");}
-//          Thông báo
-        Toast.makeText(this, String.valueOf(number1),
-                Toast.LENGTH_LONG).show();
+        tvRandom = findViewById(R.id.text_view_random);
+        tvHistory = findViewById(R.id.text_view_history);
+        btnReset = findViewById(R.id.button_reset);
+        btnRandom = findViewById(R.id.button_random);
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Click button reset", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
