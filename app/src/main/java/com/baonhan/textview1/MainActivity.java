aa7db97d11edebe0a5ef20cf2d562eab6e85d6f3
4chai <baonhanvtvn97@gmail.com>
2023-05-02 01:03:16 +0700

    done app

--- app/src/main/java/com/baonhan/textview1/MainActivity.java
+++ app/src/main/java/com/baonhan/textview1/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity
         TextView tvRandom, tvHistory;
         Button btnReset, btnRandom;
         List<String> listNames;
+        String result = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,12 +49,45 @@ public class MainActivity extends AppCompatActivity
         btnRandom.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (listNames.size()>0){
                 Random random   = new Random();
-                int indexRandom = random.nextInt(listNames.size()-1);
+                int indexRandom = random.nextInt(listNames.size());
                 String valueRandom = listNames.get(indexRandom);
                 tvRandom.setText(valueRandom);
+                tvHistory.setText(result);
+                listNames.remove(indexRandom);
+                if (listNames.size() == 1){
+                    result = valueRandom + result;
+                } else {
+                    result+= " - " +valueRandom  ;
+                }
+            } else {
+                    int Short;
+                    Toast.makeText(MainActivity.this, "Full team", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                result = "";
+                tvRandom.setText(result);
+                tvHistory.setText(result);
+                if (listNames.size()>0){
+                    listNames.clear();
+                }else{
+                    listNames.add(("Sasuke"));
+                    listNames.add("Naruto");
+                    listNames.add("Sakura");
+                    listNames.add("Kakashi");
+                    listNames.add("Hinata");
+                    listNames.add("Pain");
+                    listNames.add("Obito");
+                    listNames.add("Neji");
+                    listNames.add("Gaara");
+                    listNames.add("Rock Lee");
+                }
             }
         });
         }
-
-    }
+        }
